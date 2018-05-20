0249f62a67ca5afd7342c9ec870786e8a72fc334
florincy <36941768+florincy@users.noreply.github.com>
2018-05-19 23:30:19 -0300

    Add files via upload

---
+++ Projeto1/Projeto1/src/main/java/C1.java
@@ -0,0 +1,23 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+//Tabuada
+public class C1 {
+
+  
+    public static void main(String[] args) throws IOException {
+     int a,i,b;
+     //Criando um objeto da classe BufferedReader; 
+     BufferedReader inteiro = new BufferedReader(new InputStreamReader(System.in));
+     System.out.println("Olá, usuário! Este progama faz tabuadas de inteiros"
+     + "Digite um inteiro\n");
+     a = Integer.parseInt(inteiro.readLine());
+     for (i=0;i<=10;i++){
+     b = a*i;
+     System.out.println(+a+"x"+i+"="+b);
+     }
+    }
+    
+}
