9238a8fb48ba013355110be5eb31c99b035a5360
florincy <simoneflorincy@gmail>
2018-09-10 11:31:22 -0300

    10/09/18 ---- exs 1-3

---
+++ ex1_aula_10.09/src/main/java/Ex1.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author aluno
+ */
+public class Ex1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        for (int i = 34; i<100;i++){
+            System.out.println(i);
+        }
+        // TODO code application logic here
+    }
+    
+}
---
+++ ex1_aula_10.09/src/main/java/Ex3.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author aluno
+ */
+public class Ex3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        double b1,b2,b3,b4,media;
+        b1 = 3.5;
+        b2 = 5;
+        b3 = 7.2;
+        b4 = 9.1;
+        media = ((b1*1)+(b2*2)+(b3*3)+(b4*5))/(1+2+3+5);
+        System.out.println("Sua media é"+media);
+        
+        // TODO code application logic here
+    }
+    
+}
