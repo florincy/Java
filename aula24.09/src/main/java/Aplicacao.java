b456e4932bcf48812def882b499eabf7e26b2004
florincy <simoneflorency@gmail.com>
2018-09-24 12:18:25 -0300

    tudo 24 do 09

--- aula24.09/src/main/java/Aplicacao.java
+++ aula24.09/src/main/java/Aplicacao.java
@@ -15,34 +15,25 @@ public class Aplicacao {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        boolean estado;
-        boolean estado2;
-        ContaBancaria conta = new ContaBancaria(1234,true);
-        estado = conta.getAtiva();
+
+        ContaBancaria conta = new ContaBancaria(1234, true);
         conta.setSaldo(1500.00);
-        String a1 = "CONTA ATIVA";
-        String a2 = "CONTA  INATIVA";
-        
-        ContaBancaria conta2 = new ContaBancaria(12345,false);
-        estado2 = conta2.getAtiva();
+        ContaBancaria conta2 = new ContaBancaria(12345, false);
         conta2.setSaldo(2000.00);
-        
-        
+
         System.out.println("Informações da conta 1\n");
-        if (estado = true) {
-            System.out.println(a1);
-        } else  if (estado = false){
-            System.out.println(a2);
-        }
-        System.out.println("Número de conta:" + conta.getNumConta() + "\n" + "Saldo: R$" + conta.getSaldo() + "\n");
-        System.out.println("Informações da conta 2\n");
-        if (estado2 = true) {
-            System.out.println(a1);
-        } else if (estado = false){
-            System.out.println(a2);
-        }
-        
-        System.out.println("Número de conta:" + conta2.getNumConta() + "\n" + "Saldo: R$" + conta2.getSaldo() + "\n");
+
+        System.out.println("Conta ativa: " + conta.getAtiva() + "\n" + "Número de conta:" + conta.getNumConta() + "\n" + "Saldo: R$" + conta.getSaldo() + "\n");
+
+        System.out.println("Conta ativa: " + conta2.getAtiva() + "\n" + " Número de conta:" + conta2.getNumConta() + "\n" + "Saldo: R$" + conta2.getSaldo() + "\n");
+
+        conta.sacar(110.00);
+        conta.depositar(200.00);
+        System.out.println("Informações da conta 1\n");
+
+        System.out.println("Conta ativa: " + conta.getAtiva() + "\n" + "Número de conta:" + conta.getNumConta() + "\n" + "Saldo: R$" + conta.getSaldo() + "\n");
+
+        System.out.println("Conta ativa: " + conta2.getAtiva() + "\n" + " Número de conta:" + conta2.getNumConta() + "\n" + "Saldo: R$" + conta2.getSaldo() + "\n");
     }
 
 }
